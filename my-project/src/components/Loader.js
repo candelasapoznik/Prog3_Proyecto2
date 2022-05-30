2d91e8dda3d6ab22efaa594d088c5b5a2912bfd1
Agostina-Maldonado <you@example.com>
2022-05-30 19:30:04 -0300

    limpiando el app.js y creación del leader.js

---
+++ my-project/src/components/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {ActivityIndicator} from 'react-native';
+
+function Loader() {
+  return (
+    <ActivityIndicator size="big" color="red" />
+  )
+}
+
+export default Loader;
