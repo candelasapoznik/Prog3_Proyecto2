1c0dfef9de3dc1f8367566de04b425d186b56ebb
matias-maldonado <63106003+matias-maldonado@users.noreply.github.com>
2022-06-06 12:24:57 -0300

    fr

--- my-project/src/navigation/StackNav.js
+++ my-project/src/navigation/StackNav.js
@@ -1,3 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Login from "../../../src/screens/Login/Login";
+import Login from "../screens/Login";
+import Register from "../screens/Register"
+
+const stack= createNativeStackNavigator();
+
