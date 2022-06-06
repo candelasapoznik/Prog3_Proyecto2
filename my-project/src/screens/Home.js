b7e7af6d1e1aa7559e9c2f3acd254c0a7239465b
Agostina-Maldonado <you@example.com>
2022-06-06 11:46:50 -0300

    terminado userPostSearcher

--- my-project/src/screens/Home.js
+++ my-project/src/screens/Home.js
@@ -14,7 +14,7 @@ class Home extends Component {
         }
     }
     componentDidMount(){
-            db.collection('posts').onSnapshot(
+            db.collection('posts').orderBy('createdAt', 'desc').onSnapshot(
                 docs =>{
                     let posts = [];
                     docs.forEach( doc => {
