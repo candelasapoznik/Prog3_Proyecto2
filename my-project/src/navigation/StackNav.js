b1e6c4b037a76ed4038e17a8cefb418c9cfc173a
matias-maldonado <63106003+matias-maldonado@users.noreply.github.com>
2022-06-13 12:12:50 -0300

    de

--- my-project/src/navigation/StackNav.js
+++ my-project/src/navigation/StackNav.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "../screens/Login";
 import Register from "../screens/Register"
+import TabNavigation from './TabNav';
 import Home from "../screens/Home"
 import Comments from '../screens/Comments';
 
